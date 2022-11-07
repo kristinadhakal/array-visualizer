2e8fb7bbecfe4be24d118011eeb38a0452c757c0
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-11-07 14:11:25 -0700

    ello

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hi");
+// Array Visulaizer
+
+// Global Variable
+let myData = [400, 500, 250, 550, 100, 300];
+
+// Display Data
+drawArray();
+
+function drawArray() {
+  for (let i = 0; i < myData.length; i++) {
+    console.log(myData[i]);
+  }
+}
