b9bab5f5c0c58a2a95833ee52cd7a0c82dbf2198
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-11-22 14:33:26 -0700

    hiu

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let containerEL = document.getElementById("container");
 
 // Global Variable
 let myData = [];
-for (let n = 1;  n <= 50; n++){
+for (let n = 1; n <= 50; n++) {
   myData.push(Math.random() * 600);
 }
 console.log(myData);
@@ -14,24 +14,37 @@ console.log(myData);
 drawArray();
 
 function drawArray() {
-  let outputStr =  "";
+  let outputStr = "";
   for (let i = 0; i < myData.length; i++) {
-    outputStr += `<div style="height: ${myData[i]}px"></div>`
-    }
+    outputStr += `<div style="height: ${myData[i]}px"></div>`;
+  }
 
-    containerEL.innerHTML = outputStr;
+  containerEL.innerHTML = outputStr;
 }
 
 // Key Event Stuff
 document.addEventListener("keydown", keydownHandler);
 
-function keydownHandler(event){
-// Modify the array
-for (let i = 0; i < myData.length; i++) {
-    myData[i] = 400;
-
+function keydownHandler(event) {
+  if (event.keyCode === 38) {
+    // Up Array -  AA 600 to the idex 0
+    let middleIndex = Math.floor(myData.length / 2);
+    myData.splice(middleIndex, 0, Math.random() * 600);
+  } else if (event.keyCode === 40) {
+    // Down Arrow - Remove element from middle
+    let middleIndex = Math.floor(myData.length / 2);
+    myData.splice(middleIndex, 1);
   }
-  // Redraw the array 
-drawArray()
 
+  // if (event.keyCode === 38) {
+  //   // Up Array -  Push Value to end of array
+  //   myData.push(Math.random() * 600);
+  // } else if (event.keyCode === 40) {
+  //   // Down Arrow - Remove value from the end of the array
+  //   let poppedValue = myData.pop();
+  //   console.log(poppedValue);
+  // }
+
+  // Redraw the array
+  drawArray();
 }
